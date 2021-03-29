c1448dbda9338197292410aad8b6d06a16cc0846
sin-2020 <74129617+sin-2020@users.noreply.github.com>
2021-03-28 23:42:46 -0700

    Updated

---
+++ js/Script.js
@@ -0,0 +1,27 @@
+const selectElement = (element) =>document.querySelector(element);
+
+selectElement('.mobile-menu').addEventListener('click' , ()=>{
+selectElement('header').classList.toggle('active')
+})
+
+if ($(window).width() <= 768){	
+    $('.footer-links-wrapper').addClass("someClass");
+}else{
+    $('.footer-links-wrapper').removeClass("someClass");
+}
+$(window).on('resize', function(event){
+    if ($(window).width() <= 768){	
+          $('.footer-links-wrapper').addClass("someClass");
+    }else{
+          $('.footer-links-wrapper').removeClass("someClass");
+          $('.footer-links-wrapper ul').show();
+    }	
+});
+
+// Footer collapse functionality 
+$(document).on("click", ".someClass h3", function(){
+    $(this).next('ul').slideToggle();
+    $(this).toggleClass("expanded");
+    
+});
+
